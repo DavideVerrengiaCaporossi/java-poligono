679df9b84cde8120680d9b8bd43b3c087cc2d5d6
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-06-28 16:56:28 +0200

    fine esercizio

--- src/Jana60/Main.java
+++ src/Jana60/Main.java
@@ -3,8 +3,17 @@ package Jana60;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//test istanza per rettangolo
+		System.out.println("test Rettangolo");
+		Rettangolo rett1 = new Rettangolo(8,12);
+		System.out.println(rett1);
+		
+		//test istanza per Triangolo
+		System.out.println("test Triangolo");
+		Triangolo tria1 = new Triangolo(3,5,2,3);
+		System.out.println(tria1);
+		
 
-	}
-
+}
 }
